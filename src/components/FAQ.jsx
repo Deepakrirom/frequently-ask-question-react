6891d2a56bb84c443b601ebc640ad0b6bb60c0c6
Deepakumar Irom <Deepakirom171@gmail.com>
2026-06-05 00:05:38 +0530

    Fix layout by adjusting div height in FAQ component

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,7 +3,7 @@ import faqData from "../data/data";
 
 const FAQ = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-900 p-3 ">
+    <div className="w-full h-full flex justify-center items-center bg-gray-900 p-3 ">
       <div className="max-w-2xl w-full p-6 bg-amber-100 rounded-2xl">
         <h1 className="text-2xl font-bold text-center mb-3">
           FAQ
